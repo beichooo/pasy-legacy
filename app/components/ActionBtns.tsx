d6d3c6fe5992c37bf760765718536a5ef0c2d458
beichooo <be.imagine.redo@gmail.com>
2024-04-13 18:02:41 -0400

    Create the frontend of searcher form and add some dependencies

---
+++ app/components/ActionBtns.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  type: "submit" | "button";
+  variant: "primary" | "secondary";
+}
+
+const ActionBtns: React.FC<ButtonProps> = ({ text, type, variant }) => {
+  const baseClasses = "flex-1 rounded-2xl font-medium text-xl p-3";
+  const primaryClasses = "bg-pasy-brown-text text-white";
+  const secondaryClasses =
+    "text-pasy-brown-text border-2 border-pasy-brown-text";
+
+  const classes = `${baseClasses} ${
+    variant === "primary" ? primaryClasses : secondaryClasses
+  }`;
+
+  return (
+    <button type={type} className={classes}>
+      {text}
+    </button>
+  );
+};
+
+export default ActionBtns;
